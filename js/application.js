3f77ac60853d5b275748208aa0761abc8297e69f
William Belle <william.belle@gmail.com>
2013-06-03 20:56:12 +0200

    add form layout for motorcycle

--- js/application.js
+++ js/application.js
@@ -20,8 +20,21 @@
       var taxeVaud = new TaxeVaud();
 
       var result = taxeVaud.truck(
-        $('#trucktaxform [name=weight]').val(),
+        $('input[name=weight]', '#trucktaxform').val(),
         $('input[name=euroNorme]:checked', '#trucktaxform').val()
+      ); 
+       
+      $('.result').text(result);
+      $('.result-container').show();
+    });
+    
+    $(document).delegate( '#motorcycleTax', 'tap', function(event){
+      event.preventDefault();
+      
+      var taxeVaud = new TaxeVaud();
+
+      var result = taxeVaud.motorcycle(
+        $('input[name=cm3]', '#motorcycletaxform').val()
       ); 
        
       $('.result').text(result);
